2858c2f0f37362a497f09fec3d8c567820000456
Tyler Eck <tylereck81@yahoo.com>
2025-11-16 13:14:27 +0100

    Added Cache to reduce loading time

--- clocklife.py
+++ clocklife.py
@@ -1,12 +1,24 @@
+"""
+Tyler Eck 
+Clock Life 
+Simple program to translate/convert your age in military time/clock format. It gives insights into where you are in the day based on your age, gender, and country. 
+
+"""
 from bs4 import BeautifulSoup 
 from urllib.request import urlopen 
 from time import strftime
 from datetime import datetime
+import json
+import os
+from pathlib import Path
 
-url = "https://www.worldometers.info/demographics/life-expectancy/"
+url = "https://www.worldometers.info/demographics/life-expectancy/" # URL to get data
+cache = Path("life_expectancy_cache.json") # cache to fetch data from
+MAX_CACHE_AGE_DAYS = 30 # cache date in days, reloads data every 30 days
 
-
+# parse website to get life expectancy data 
 def parse_website(url): 
+    print("ACCESSING WEBSITE")
     data = []
     page = urlopen(url)
     html = page.read().decode("utf-8")
@@ -22,7 +34,7 @@ def parse_website(url):
 
     return data
 
-
+# receive country input from user
 def get_country(data): 
     countries = [d[1] for d in data]
     countries.sort()
@@ -31,8 +43,8 @@ def get_country(data):
         
     selection = ""
     while selection == "": 
-        selection = input("Enter your country from list: ")
-        if selection.strip().lower() not in [c.lower() for c in countries]: 
+        selection = input("Enter your country from list: ").strip()
+        if selection.lower() not in [c.lower() for c in countries]: 
             print("Error, not a valid country")
             selection = ""
         else:
@@ -42,6 +54,7 @@ def get_country(data):
         if d[1].lower()  == selection.lower():
             return d
 
+# receive date of birth input from user 
 def get_DOB(): 
     while True: 
         date = input("Enter your date of birth in format day/month/year: ")
@@ -61,12 +74,13 @@ def get_DOB():
     
     return datetime.strptime(date,"%d/%m/%Y")
 
+# receive gender input from user
 def get_gender(): 
     selection = ""
 
     while selection == "": 
-        selection = input("Enter Gender (Male or Female): ")
-        if selection.strip().lower() != "male" and selection.strip().lower() != "female":
+        selection = input("Enter Gender (Male or Female): ").strip().lower()
+        if selection != "male" and selection != "female":
             print("Error, not valid input")
             selection = ""
         else:
@@ -74,23 +88,55 @@ def get_gender():
     
     return 3 if selection.strip().lower() == "female" else 4
 
+# load data either from cache or from website
+def load_data(): 
+
+    if cache.exists(): 
+        mtime = datetime.fromtimestamp(cache.stat().st_mtime)
+        age_days = (datetime.now() - mtime).days
+
+        if age_days <=MAX_CACHE_AGE_DAYS: 
+            print("Accessing Cache")
+            with open(cache,"r", encoding = "utf-8") as f: 
+                data= json.load(f)
+        else: 
+            print(f"Cache is {age_days} days old. Refreshing from web")
+            data = parse_website(url)
+
+            with open(cache, "w", encoding="utf-8") as f: 
+                json.dump(data, f, ensure_ascii=False, indent = 2)
+            
+    else: 
+        data = parse_website(url)
+
+        with open(cache, "w", encoding="utf-8") as f: 
+            json.dump(data, f, ensure_ascii=False, indent = 2)
+    
+    return data
 
 def main():
     # Get Data 
-    data = parse_website(url)
+    data = load_data()
     country_data = get_country(data)
     today_date = datetime.today()
     birth_day = get_DOB()
     diff = today_date - birth_day
     age = round(float(diff.days/365),1)
-    life_expectancy = float(country_data[get_gender()])
+    gender = get_gender()
+    life_expectancy = float(country_data[gender])
     time = (age/life_expectancy)*24
+
+    print("TOday date: ",today_date)
+    print("Birthday: ", birth_day)
+    print("Age: ", age)
+    print("Life exp: ", life_expectancy)
+    print("Time: ", time)
 
     hour = int(time)
     min = round((time - hour)*60)
     if min <10: min = str("0"+str(min))
     print("Time: ",hour,":",min)
-
+    
 
 if __name__ == "__main__": 
     main()
